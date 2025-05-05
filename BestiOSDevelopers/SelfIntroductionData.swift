6f3e6d6e335c08021e6b9105ea8bef8e1246860d
계주성 <kyejusung@gmail.com>
2025-05-05 13:33:44 +0900

    SK

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -417,7 +417,7 @@ let people: [SelfIntroduction] = [
         strength: "끈기와 실행력",
         sns: nil,
         etc: "열심히 수료하고 취업하겠습니다!",
-        job: "SK - iOS 개발자"
+        job: "SK iOS 개발자"
     ),
     SelfIntroduction(
         name: "정소이",
